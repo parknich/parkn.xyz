c67fcc12d07336d9657068dfd3624aec19b32c46
parknich081 <parknich082@gmail.com>
2024-07-12 00:02:02 -0400

    Fix https maybe?

--- frontend/server.js
+++ frontend/server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Create HTTPS server
