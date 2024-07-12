684e288dbe008fb5338aaa114665b0d970cf6412
parknich081 <parknich082@gmail.com>
2024-07-12 00:03:31 -0400

    Fix https maybe?

--- frontend/server.js
+++ frontend/server.js
@@ -22,6 +22,8 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
