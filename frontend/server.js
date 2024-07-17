550b1164490a1fadb4196914112fb7bc6757dc48
parknich081 <parknich082@gmail.com>
2024-07-17 12:32:12 -0400

    change to hello world tmep

--- frontend/server.js
+++ frontend/server.js
@@ -4,7 +4,12 @@ const path = require('path');
 const app = express();
 const PORT =  3000
 
-app.use(express.static(path.join(__dirname, 'build')));
+//app.use(express.static(path.join(__dirname, 'build')));
+
+app.get("/", (req,res, next)=>{
+    res.send("hello world")
+    }
+)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
