85933dbcbd118bdb69f8a4e698ed99ebf7647ec7
parknich081 <parknich082@gmail.com>
2024-07-17 12:26:12 -0400

    t

--- frontend/server.js
+++ frontend/server.js
@@ -3,12 +3,6 @@ const path = require('path');
 
 const app = express();
 const PORT =  3000
-app.use((req, res, next) => {
-    if (!req.secure) {
-        return res.redirect(`https://${req.headers.host}${req.url}`);
-   }
-   next();
-});
 
 app.use(express.static(path.join(__dirname, 'build')));
 
