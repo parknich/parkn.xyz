fbbdc152d281b200a1932328edda9b8d1b2bdf5b
parknich081 <parknich082@gmail.com>
2024-07-17 13:11:49 -0400

    add catch all for index html

--- frontend/server.js
+++ frontend/server.js
@@ -6,6 +6,10 @@ const PORT =  3000
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
