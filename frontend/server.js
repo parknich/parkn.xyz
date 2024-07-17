36512f45e2240f86108b6bd109c04f098a9b5579
parknich081 <parknich082@gmail.com>
2024-07-17 13:14:09 -0400

    why is it looking in frontend and not build ???? bandaid fix

--- frontend/server.js
+++ frontend/server.js
@@ -7,7 +7,7 @@ const PORT =  3000
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
 app.listen(PORT, () => {
