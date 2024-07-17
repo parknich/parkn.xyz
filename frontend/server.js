696b1f5f4711056fe002b3b83479ddcb88a99ecf
parknich081 <parknich082@gmail.com>
2024-07-17 11:37:04 -0400

    Remove https stuff

--- frontend/server.js
+++ frontend/server.js
@@ -1,29 +1,17 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const HTTPS_PORT = 443;
-const PORT =  421
-
-const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/parknich.xyz/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/parknich.xyz/fullchain.pem')
-};
-
-app.use((req, res, next) => {
-    if (!req.secure) {
-        return res.redirect(`https://${req.headers.host}${req.url}`);
-    }
-    next();
-});
+const PORT =  3000
+//app.use((req, res, next) => {
+//    if (!req.secure) {
+//        return res.redirect(`http://${req.headers.host}${req.url}`);
+//    }
+//    next();
+//});
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-https.createServer(options, app).listen(HTTPS_PORT, () => {
-    console.log(`HTTPS Server is running on port ${HTTPS_PORT}`);
-});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
